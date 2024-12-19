80969b88dcc9da600e765d88390b0532f3624195
Prannay Kedia <prannaykedia1@gmail.com>
2024-12-20 00:38:54 +0530

    Added dispay of blog id

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,11 @@ export default defineConfig({
     schema: {
       NODE_ENV: envField.string({ context: "server", access: "public" }),
       SMTP_USER: envField.string({ context: "server", access: "public" }),
-      SMTP_PASSWORD: envField.string({ context: "server", access: "public" }),
+      SMTP_PASSWORD: envField.string({ context: "server", access: "secret" }),
+      MONGO_URI: envField.string({ context: "server", access: "secret" }),
+      MONGO_DB_ID: envField.string({ context: "server", access: "public" }),
       APP_URL: envField.string({ context: "server", access: "public" }),
-      NOTION_KEY: envField.string({ context: "server", access: "public" }),
+      NOTION_KEY: envField.string({ context: "server", access: "secret" }),
       NOTION_BLOG_ID: envField.string({ context: "server", access: "public" }),
       NOTION_TAG_ID: envField.string({
         context: "server",
---
+++ src/services/website.ts
@@ -0,0 +1,9 @@
+import connectMongo from "./mongoConnect";
+
+// export async function getWebsiteFromId(id: string): Promise<IWebsite> {
+//   // TODO: Add error handling
+//   await connectMongo();
+//   const website = (await Website.findById(id)) as IWebsite;
+
+//   return website;
+// }
